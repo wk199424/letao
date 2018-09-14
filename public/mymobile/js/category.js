0f918c7425a00379151112767f55b19f56e23179
wk <wangkui4052@qq.com>
2018-09-14 16:41:21 +0800

    分类页面完成

--- public/mymobile/js/category.js
+++ public/mymobile/js/category.js
@@ -5,32 +5,41 @@ $(function () {
 
 
     $.ajax({
-        url:"/category/queryTopCategory",
-        type:"get",
-        success: function(response){
+        url: "/category/queryTopCategory",
+        type: "get",
+        success: function (response) {
             console.log(response);
-            var html = template('category-first',{result:response.rows});
+            var html = template('category-first', {result: response.rows});
             $('#links').html(html);
+            if (response.rows.length) {
+                $('#links').find('a').eq(0).addClass('active');
+                var id = response.rows[0].id;
+                getSecondCategory(id);
+            }
 
         }
     });
 
 
-
-    $('#links').on('click','a',function () {
+    $('#links').on('click', 'a', function () {
         console.log($(this).attr('data-id'))
-       var id = $(this).attr('data-id');
-        $.ajax({
-            url:'/category/querySecondCategory',
-            type:'get',
-            data:{
-                id:id
-            },
-            success: function(response){
-                console.log(response)
-                var html = template('category-second',response);
-                $('#cont-right').html(html);
-            }
-        })
+        var id = $(this).attr('data-id');
+        $(this).addClass('active').siblings().removeClass('active');
+        getSecondCategory(id);
     });
 });
+
+function getSecondCategory(id) {
+    $.ajax({
+        url: '/category/querySecondCategory',
+        type: 'get',
+        data: {
+            id: id
+        },
+        success: function (response) {
+            console.log(response)
+            var html = template('category-second', response);
+            $('#cont-right').html(html);
+        }
+    })
+}
